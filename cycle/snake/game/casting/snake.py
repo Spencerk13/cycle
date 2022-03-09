5a3e0e9c77b093360cee3164e8e2c5bd85ab23f4
Spencerk13 <spen.kingsbu@gmail.com>
2022-03-09 13:50:56 -0800

    done

---
+++ cycle/snake/game/casting/snake.py
@@ -0,0 +1,76 @@
+from decimal import MAX_EMAX
+import constants
+import random
+from game.casting.actor import Actor
+from game.shared.point import Point
+from game.casting.cast import Cast
+import pyray
+
+class Snake(Actor):
+    """
+    A long limbless reptile.
+    
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
+    def __init__(self):
+        super().__init__()
+        self._segments = []
+        self._prepare_body()
+
+    def get_segments(self):
+        return self._segments
+
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
+    def get_head(self):
+        return self._segments[0]
+
+    def grow_tail(self):
+        if constants.SNAKE_LENGTH < 150:
+            tail = self._segments[-1]
+            velocity = tail.get_velocity()
+            offset = velocity.reverse()
+            position = tail.get_position().add(offset)
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text("#")
+            segment.set_color(constants.GREEN)
+            self._segments.append(segment)
+            constants.SNAKE_LENGTH+=1
+
+    def turn_head(self, velocity):
+        self._segments[0].set_velocity(velocity)
+
+    def create_rect(self,object):
+        return pyray.Rectangle(object.get_position().get_x(),object.get_position().get_y(),10,10)
+
+    def _prepare_body(self):
+        x = random.randint(0,900)
+        y = random.randint(0,600)
+
+        for i in range(constants.SNAKE_LENGTH):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0)
+            text = "8" if i == 0 else "#"
+            color = constants.YELLOW if i == 0 else constants.GREEN
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(color)
+            self._segments.append(segment)
---
+++ cycle/snake/game/scripting/handle_collisions_action.py
@@ -0,0 +1,108 @@
+import constants
+from game.casting.actor import Actor
+from game.scripting.action import Action
+from game.shared.point import Point
+import pyray
+
+class HandleCollisionsAction(Action):
+    """
+    An update action that handles interactions between the actors.
+    
+    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
+    with the food, or the snake collides with its segments, or the game is over.
+
+    Attributes:
+        _is_game_over (boolean): Whether or not the game is over.
+    """
+
+    def __init__(self):
+        """Constructs a new HandleCollisionsAction."""
+        self._is_game_over = False
+        self._winner = 0
+
+    def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            self._handle_player_collision(cast)
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
+
+    def _handle_player_collision(self, cast):
+        """Updates the score nd moves the food if the snake collides with the food.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        snake = cast.get_first_actor("snakes")
+        head = snake.get_head()
+        segments = snake.get_segments()[1:]
+        p2 = cast.get_first_actor("p2")
+        head2 = p2.get_head()
+        segments2 = p2.get_segments()[1:]
+        for segment in segments:
+            rect1 = p2.create_rect(head2)
+            rect2 = snake.create_rect(segment)
+            if pyray.check_collision_recs(rect1,rect2):
+                self._is_game_over = True
+                self._winner=1
+        for segment in segments2:
+            rect1 = snake.create_rect(head)
+            rect2 = p2.create_rect(segment)
+            if pyray.check_collision_recs(rect1,rect2):
+                self._is_game_over = True
+                self._winner=0
+    
+    def _handle_segment_collision(self, cast):
+        """Sets the game over flag if the snake collides with one of its segments.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        snake = cast.get_first_actor("snakes")
+        head = snake.get_segments()[0]
+        segments = snake.get_segments()[1:]
+        
+        p2 = cast.get_first_actor("p2")
+        head2 = p2.get_segments()[0]
+        segments2 = p2.get_segments()[1:]
+
+        for segment in segments:
+            if head.get_position().equals(segment.get_position()):
+                self._is_game_over = True
+                self._winner=0
+        for segment in segments2:
+            if head2.get_position().equals(segment.get_position()):
+                self._is_game_over = True
+                self._winner=1
+    def _handle_game_over(self, cast):
+        """Shows the 'game over' message and turns the snake and food white if the game is over.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        if self._is_game_over:
+            snake = cast.get_first_actor("snakes")
+            segments = snake.get_segments()
+            p2 = cast.get_first_actor("p2")
+            segments2 = p2.get_segments()
+            food = cast.get_first_actor("foods")
+
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+            position = Point(x, y)
+
+            message = Actor()
+            message.set_text("Game Over!")
+            message.set_position(position)
+            cast.add_actor("messages", message)
+            if self._winner == 0:
+                for segment in segments:
+                    segment.set_color(constants.WHITE)
+            else:
+              for segment in segments2:
+                    segment.set_color(constants.WHITE)
